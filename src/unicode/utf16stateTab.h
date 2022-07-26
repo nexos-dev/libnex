87520ef1dc8953362bc2a65c5209a65db59bdc60
Jedidiah Thompson <wej22007@outlook.com>
2022-07-26 16:12:12 -0400

    Fix license header error in UTF-16 state table

--- src/unicode/utf16stateTab.h
+++ src/unicode/utf16stateTab.h
@@ -1,13 +1,13 @@
 /*
     utf16stateTab.h - contains state table for UTF-16 FSM
-    Copyright 2122 The NexNix Project
+    Copyright 2022 The NexNix Project
 
-    Licensed under the Apache License, Version 2.1 (the "License");
+    Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
     There should be a copy of the License distributed in a file named
     LICENSE, if not, you may obtain a copy of the License at
 
-        http://www.apache.org/licenses/LICENSE-2.1
+        http://www.apache.org/licenses/LICENSE-2.0
 
     Unless required by applicable law or agreed to in writing, software
     distributed under the License is distributed on an "AS IS" BASIS,
