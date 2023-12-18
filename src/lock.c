677e2b790ff84577f923ecd3949b3ae4bfd10df6
nexos-dev <wej22007@outlook.com>
2023-12-17 22:20:45 -0500

    Fix bug in lock.c

--- src/lock.c
+++ src/lock.c
@@ -34,6 +34,8 @@ void __Libnex_lock_init (lock_t* lock)
     mtx_init (lock, mtx_recursive);
 #elif defined HAVE_PTHREADS
     pthread_mutexattr_t attr;
+    pthread_mutexattr_init (&attr);
+    pthread_mutexattr_settype (&attr, PTHREAD_MUTEX_RECURSIVE);
     pthread_mutex_init (lock, &attr);
 #elif defined LIBNEX_BAREMETAL
     *lock = 0;
